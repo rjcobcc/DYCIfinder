c5f836cc39fe13b718182d9e4cbb28a1120909d6
JcobC <arjhaycelestino54@gmail.com>
2026-04-09 17:08:52 +0800

    add phpmailer library

--- app/conf/api.js
+++ app/conf/api.js
@@ -1,9 +1,2 @@
-export const PROJECT = "http://localhost/DYCIfinder";
-export const APIs = PROJECT + "/api";
-
-export const EDIT_ITEM = APIs + "/edit_item.php";
-export const POST_CLAIM = APIs + "/post_claim.php";
-export const POST_FOUND = APIs + "/post_found.php";
-export const POST_LOST = APIs + "/post_lost.php";
-export const REQUEST_ACCOUNT = APIs + "/request_account.php";
-export const VERIFY_USER = APIs + "/verify_user.php";
+const PROJECT_LINK = "http://localhost/DYCIfinder";
+export const API = PROJECT_LINK + "/api";
---
+++ lib/emailer.php
@@ -0,0 +1,33 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+require_once __DIR__ . '/../conf/email.php';
+
+function sendEmail($to, $subject, $body) {
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = PROJECT_EMAIL;
+        $mail->Password   = EMAIL_PASS;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+        $mail->setFrom(PROJECT_EMAIL, PROJECT_NAME);
+        $mail->addAddress($to);
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+        $mail->AltBody = strip_tags($body);
+        $mail->send();
+        return true;
+    } 
+    catch (Exception $e) {
+        return false;
+    }
+}
+?>
